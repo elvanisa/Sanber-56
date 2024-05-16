09ec0614860d9a7521ef226a392fb3065f8da089
elvanisa <elvanisamuhsina@gmail.com>
2024-05-16 19:37:47 +0700

    kasirAja Login

---
+++ cypress/e2e/web-test/kasirAja.cy.js
@@ -0,0 +1,29 @@
+describe('template spec', () => {
+  beforeEach(() => {
+    cy.visit('https://kasirdemo.belajarqa.com/')
+  })
+  it('Failed Login - Wrong Credentials', () => {
+    cy.get('#email').type('salah@gmail.com')
+    cy.get('#password').type('salah')
+    cy.get('.chakra-button').click()
+    cy.wait(100)
+    cy.get('.chakra-alert').should('contain.text','Kredensial yang Anda berikan salah')
+  })
+  it('Failed Login - Invalid Email', () => {
+    cy.get('#email').type('salah')
+    cy.get('#password').type('salah')
+    cy.get('.chakra-button').click()
+    cy.get('.chakra-alert').should('contain.text','must be a valid email')
+  })
+  it('Failed Login - Empty Password', () => {
+    cy.get('#email').type('elva@gmail.com')
+    cy.get('.chakra-button').click()
+    cy.get('.chakra-alert').should('contain.text','not allowed to be empty')
+  })
+  it('Success Login', () => {
+    cy.get('#email').type('elva@gmail.com')
+    cy.get('#password').type('elva')
+    cy.get('.chakra-button').click()
+    cy.url().should('include','dashboard')
+  })
+})
